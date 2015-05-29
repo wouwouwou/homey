4ead44a0360d9edcd665ce28610b79803184271b
Tim Hintzbergen <t.a.h.hintzbergen@student.utwente.nl>
2015-05-29 20:38:45 +0200

    reworked file to tutorial files, still uncaught errors

--- timSwek/app.js
+++ timSwek/app.js
@@ -1,19 +1,32 @@
-var App = new Object();
-App.init = function() {
-	
+"use strict";
+
+function App() {
+
 }
 
+// Lets the Homey speak 'text' and logs if true
 function speakAndLog(text, log) {
-	Homey.manager("speech-output").say(text);
+	Homey.manager('speech-output').say(text);
 	if (log) {
 		Homey.log(text);
 	}
 }
 
+module.exports = App;
+
+App.prototype.init = function() {
+		Homey.manager('ledring').animate({
+		name: 'pulse'
+	});
+}
+
+
 // Rolls a single die with of the type (sides) given
 function rollDie(sides) {
 	return 1 + Math.floor(Math.random() * sides);	
 }
+
+
 
 // Rolls multiple dice specified by amount and of type sides; Uses rollDie()
 function rollDice(sides, amount, log) {
@@ -31,13 +44,13 @@ function rollDice(sides, amount, log) {
 }
 
 
-App.prototype.speech = function(speech) {
+App.prototype.speech = function( speech ) {
+	var log = true
 	
 	// loop all triggers
 	speech.triggers.forEach(function(trigger) {
 		if (trigger.id == 'throw a d6') {
-			//Do the roll
-			rollDice(6, 1, true);
+			rollDice(6, 1, log);
 			
 			
 			// Obviously, you would get the real weather here
