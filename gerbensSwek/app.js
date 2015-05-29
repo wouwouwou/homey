e7ab2bb8658819dd25eaff32617542c990c69aba
Gerryflap <gerry2211@gmail.com>
2015-05-29 23:35:16 +0200

    Wiki search werkt

--- gerbensSwek/app.js
+++ gerbensSwek/app.js
@@ -1,5 +1,7 @@
 "use strict";
 
+var request = require('request');
+
 function App(){
 
 
@@ -13,31 +15,62 @@ function speakAndLog(text, log){
 	
 }
 
+function toTitleCase(str)
+{
+    return str.replace(/\w\S*/g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();});
+}
+
+function rollDie(numberOfSides){
+	return Math.floor(Math.random()*numberOfSides + 1, 0)
+}
+
+function getInformation(name){
+	
+	name = toTitleCase(name);
+	request.get("https://en.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&exintro=&explaintext=&titles="+name
+				, function(error, response, body){
+		
+		var object = JSON.parse(body);
+		if(object.query != undefined){
+			if(object.query.pages != undefined){
+				if(Object.keys(object.query.pages).length > 0){
+					var extract = object.query.pages[Object.keys(object.query.pages)[0]].extract;
+					if(extract != undefined){
+						speakAndLog("Some information about "+ name + ". \n" + extract.split("\n")[0], true);
+					} else {
+						speakAndLog(extract, true);
+					}
+				} else {
+					speakAndLog("No info found!", true);
+				}
+			}
+		}
+		
+	});
+
+	
+	
+	
+}
+
 module.exports = App;
 
 App.prototype.init = function(){
 	
-	Homey.manager('ledring').animate({
-		name: 'pulse'
-	});
   
 };
 
 App.prototype.speech = function( speech ) {
 		var log = true;
-	
+		Homey.log(speech);
 	    speech.triggers.forEach(function(trigger){
-        if( trigger.id == 'generated6' ) {
-            
-            // speak the weather
-			speakAndLog("I rolled the number " + Math.floor(Math.random()*6 + 1, 0), log);
-            // Obviously, you would get the real weather here
-            // And in case you didn't know yet, __() is for translation
-            
-        } else if( trigger.id == 'tomorrow' ) {
+        if( trigger.id == 'giveinfo' ) {
+			var transcript = speech.transcript;
+			getInformation(transcript.replace("give information about ", ""));
+			
             // ...	
         } else {
-				
+			getInformation("Google");
 		}
         
     });
