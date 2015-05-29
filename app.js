8c814a6e511f1b90b91e7c2c9b551b73a836c734
Gerryflap <gerry2211@gmail.com>
2015-05-30 00:33:51 +0200

    Fixed everything

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 "use strict";
 
+var request = require('request');
+
 function App(){
 	var self = this;
 
@@ -30,9 +32,14 @@ function App(){
 				self.move(8);
 			} else if (trigger.id == 'moveNine') {
 				self.move(9);
-			}
-		});
-	}
+			} else if( trigger.id == 'giveinfo' ) {
+			var transcript = speech.transcript;
+			getInformation(transcript.replace("give information about ", ""));
+        } 
+        
+    });
+}
+
 
 	this.move = function(field) {
 		if (self.game == null) {
@@ -75,6 +82,55 @@ function App(){
 		Homey.log(text);
 		Homey.manager('speech-output').say(text);
 	}
+}
+
+function speakAndLog(text, log){
+	Homey.manager('speech-output').say(text);
+	if (log){
+		Homey.log(text);	
+	}
+	
+}
+
+function toTitleCase(str)
+{
+    return str.replace(/\w\S*/g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();});
+}
+
+function rollDie(numberOfSides){
+	return Math.floor(Math.random()*numberOfSides + 1, 0)
+}
+
+function getInformation(name){
+	
+	name = toTitleCase(name);
+	request.get("https://en.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&exintro=&explaintext=&titles="+name
+				, function(error, response, body){
+		
+		var object = JSON.parse(body);
+		if(object.query != undefined){
+			if(object.query.pages != undefined){
+				if(Object.keys(object.query.pages).length > 0){
+					var extract = object.query.pages[Object.keys(object.query.pages)[0]].extract;
+					if(extract != undefined){
+						speakAndLog("Some information about "+ name + ". \n" + extract.split("\n")[0], true);
+					} else {
+						speakAndLog("Some information about "+ name + ". \n" + extract, true);
+					}
+				} else {
+					speakAndLog("No info found!", true);
+				}				
+				
+			} else {
+				speakAndLog("No info found!", true);
+			}			
+			
+		} else {
+			speakAndLog("No info found!", true);
+		}
+		
+	});
+	
 }
 
 function Game() {
--- timSwek/README.md
+++
@@ -1,2 +0,0 @@
-# Spellcraft
-Spellcraft is an app that uses the homey to throw virtual dice of all kind!
