1bbdd63b59f0d678ca9157104206a5826108d2e2
Gerryflap <gerry2211@gmail.com>
2015-05-29 19:18:49 +0200

    Made it better

--- app.js
+++ app.js
@@ -1,12 +1,36 @@
+"use strict";
+
 function App(){
-	this.init = function(){
-		for(var i = 0; i < 10; i++){
-			console.log(i);
-		}
-	}
-	this.count = function(){
-		for(var i = 0; i < 10; i++){
-			console.log(i);
-		}
-	}
+
+}
+
+module.exports = App;
+
+App.prototype.init = function(){
+	
+  	setInterval(function(){
+		Homey.log('Hoi Mick!');
+	}, 1000);
+	
+	Homey.manager('ledring').animate({
+		name: 'pulse'
+	});
+  
+};
+
+App.prototype.speech = function( speech ) {
+	    speech.triggers.forEach(function(trigger){
+        if( trigger.id == 'count' ) {
+            
+            // speak the weather
+            Homey.manager('speech-output').say(("The weather today is amazing!") );
+            
+            // Obviously, you would get the real weather here
+            // And in case you didn't know yet, __() is for translation
+            
+        } else if( trigger.id == 'tomorrow' ) {
+            // ...	
+        }
+        
+    });
 }
