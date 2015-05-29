7ec057c2126a66e380006022777a865a3bec531b
Gerryflap <gerry2211@gmail.com>
2015-05-29 18:48:24 +0200

    Het werkt

---
+++ app.js
@@ -0,0 +1,12 @@
+function App(){
+	this.init = function(){
+		for(var i = 0; i < 10; i++){
+			console.log(i);
+		}
+	}
+	this.count = function(){
+		for(var i = 0; i < 10; i++){
+			console.log(i);
+		}
+	}
+}
